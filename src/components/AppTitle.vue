f6fddd34acc81a0e42031f4582971a5efbbbd3db
markspooner89 <markspoonertkd+github@gmail.com>
2021-06-19 20:16:11 +0100

    setup base project

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 v-if="type === 'h1'">{{ text }}</h1>
+  <h2 v-else-if="type === 'h2'">{{ text }}</h2>
+</template>
+
+<script>
+export default {
+  name: "AppTitle",
+  props: {
+    type: {
+      type: String,
+      required: true,
+      validator: (val) => ["h1", "h2"].indexOf(val) > -1,
+    },
+    text: {
+      type: String,
+      required: true,
+      validator: (val) => val.trim().length > 0,
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <AppTitle type="h2" text="Welcome" />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import AppTitle from "@/components/AppTitle.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    AppTitle,
   },
 };
 </script>
