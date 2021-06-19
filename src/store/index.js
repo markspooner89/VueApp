fbeb3b1d46a865f527569a4bc3a17b0c33644ed5
markspooner89 <markspoonertkd+github@gmail.com>
2021-06-19 21:55:03 +0100

    add some state and a getter to store

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,23 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    grades: [
+      {
+        id: 1,
+        name: "White Belt",
+      },
+      {
+        id: 2,
+        name: "Yellow Tag",
+      },
+    ],
+  },
+  getters: {
+    orderedGrades: (state) => {
+      return state.grades.sort((a, b) => a.id - b.id);
+    },
+  },
   mutations: {},
   actions: {},
   modules: {},
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-  <AppTitle type="h2" text="Welcome" />
+  <div>
+    <AppTitle type="h2" text="Welcome" />
+    <div :key="grade.id" v-for="grade in grades">
+      <p>{{ grade.name }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import AppTitle from "@/components/AppTitle.vue";
 export default {
   name: "Home",
   components: {
     AppTitle,
   },
+  computed: mapGetters({
+    grades: "orderedGrades",
+  }),
 };
 </script>
