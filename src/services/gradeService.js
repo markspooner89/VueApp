c80531be940ca65f4c640ed144785f48b27a4e02
Mark Spooner <markspoonertkd+gitkraken@gmail.com>
2021-08-14 09:58:17 +0100

    move grade data to own file

--- src/services/gradeService.js
+++ src/services/gradeService.js
@@ -1,28 +1,15 @@
-const gradeData = [
-  {
-    id: 1,
-    name: "White Belt",
-  },
-  {
-    id: 2,
-    name: "Yellow Tag",
-  },
-  {
-    id: 3,
-    name: "Yellow Belt",
-  },
-];
+import gradeData from "../data/grades.json";
 
 const getGradesAsync = () => {
   return new Promise((resolve) => {
-    setTimeout(() => resolve(gradeData), 600);
+    setTimeout(() => resolve(gradeData), 300);
   });
 };
 
 const getGradeAsync = (id) => {
   return new Promise((resolve) => {
     const grade = gradeData.find((g) => g.id === id);
-    setTimeout(() => resolve(grade), 600);
+    setTimeout(() => resolve(grade), 300);
   });
 };
 
